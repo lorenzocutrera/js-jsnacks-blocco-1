309bb9236d22c474c6cd812cc03d26a796e83be2
Lorenzo Cutrera <lorenzoq3ra@gmail.com>
2023-08-29 18:33:36 +0200

    add logica parola corta

---
+++ jsnack2/main.js
@@ -0,0 +1,20 @@
+/*
+Snack 2:
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+*/ 
+
+
+let wordUno;
+let wordDue;
+
+wordUno = prompt('Inserisci una parola');
+console.log(wordUno.length);
+wordDue = prompt("Inserisci un'altra parola");
+console.log(wordDue.length);
+
+if(wordUno.length < wordDue.length){
+    console.log(wordUno, wordDue);
+} else if(wordDue.length < wordUno.length){
+    console.log(wordDue, wordUno)
+}
