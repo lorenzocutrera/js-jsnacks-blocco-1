5d65067af31bda06ca2e8b8e7db46599c4364f81
Lorenzo Cutrera <lorenzoq3ra@gmail.com>
2023-08-29 19:49:42 +0200

    initial commit

---
+++ jsnack3/main.js
@@ -0,0 +1,30 @@
+/*
+Snack 3:
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+let uno;
+let due;
+let tre;
+let quattro;
+let cinque;
+let sei;
+let sette; 
+let otto;
+let nove;
+let dieci;
+
+
+uno = prompt('Digita un numero');
+due = prompt('Digita un numero');
+tre = prompt('Digita un numero');
+quattro = prompt('Digita un numero');
+cinque = prompt('Digita un numero');
+sei = prompt('Digita un numero');
+sette = prompt('Digita un numero');
+otto = prompt('Digita un numero');
+nove = prompt('Digita un numero');
+dieci = prompt('Digita un numero');
+
+console.log (uno+=due+=tre+=quattro+=cinque+=sei+=sette+=otto+=nove+=dieci);
